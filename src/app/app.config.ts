72fc571cd9e5af7a47fd60a82f27144bb5a85913
Hamie Scripts <jamieclark333@gmail.com>
2025-07-20 23:11:03 +0100

    background

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
     providePrimeNG({
             theme: {
@@ -21,6 +28,13 @@ export const appConfig: ApplicationConfig = {
                     darkModeSelector: '.dark'
                 }
             }
-        })
+        }),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }).providers || []
   ],
 };
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+
+interface Language {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [SelectModule, FormsModule, TranslateModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  languages: Language[] = [];
+  selectedLanguage: Language | undefined;
 
+  ngOnInit() {
+    this.languages = [
+      { name: 'English', code: 'en' },
+      { name: 'French', code: 'fr' }
+    ];
+  }
 }
