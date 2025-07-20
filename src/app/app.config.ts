a03111005ed8b7875f1519bf0255e5e947c02e45
Hamie Scripts <jamieclark333@gmail.com>
2025-07-20 21:01:12 +0100

    app boilerplate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,12 +10,5 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }).providers!,
   ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'translator';
+
+  constructor() {
+  }
 }
